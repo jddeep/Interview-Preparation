89644710f40ce219c24eca8b9213ba746953ed16
Jaideep Prasad <jddeep003@gmail.com>
2020-08-28 14:26:34 +0530

    Create LongestIncreasingSubsequence.java

---
+++ Dynamic Programming/LongestIncreasingSubsequence.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/*
+The Longest Increasing Subsequence (LIS) problem is to find the length of the longest subsequence of a given sequence
+such that all elements of the subsequence are sorted in increasing order. For example, 
+the length of LIS for {10, 22, 9, 33, 21, 50, 41, 60, 80} is 6 and LIS is {10, 22, 33, 50, 60, 80}.
+longestIncSubs(arr[4]) = 1+max(longestIncSubs(4-1)) this is the recursive approach but this will have overlapping subproblems
+Hence we take memoized bottom up DP approach.
+*/
+class Solution
+{
+    static int longestIncSubs(int[] arr){
+        int n = arr.length;
+        if(n == 0) return 0;
+        int[] lenArr = new int[n];
+        lenArr[0] = 1;
+        int ans = 0; //this has the max/longest length of any subsequence throughout the arr
+        for(int i = 1;i<n;i++){
+            lenArr[i] =1; //any element on its own without any support has length 1 since its largest in itself
+            for(int j = 0;j<i;j++){
+                if(arr[i]>arr[j]){
+                    lenArr[i] = Math.max(lenArr[i], lenArr[j]+1);
+                }
+            }
+            if(lenArr[i]>ans) ans = lenArr[i];
+        }
+        return ans;
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println(longestIncSubs(new int[]{3, 10, 2, 1, 20}));
+	}
+}
